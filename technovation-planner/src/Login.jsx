f39e3a781883bea82bf6b01c926897d384802988
June Qin <juneqin2017@gmail.com>
2025-05-17 22:54:10 -0700

    connect login frontend and backend

--- technovation-planner/src/Login.jsx
+++ technovation-planner/src/Login.jsx
@@ -1,19 +1,41 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { auth } from './configuration'
+import { signInWithEmailAndPassword } from "firebase/auth";
 import './Login.css'
 
 function Login() {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setError('');
+        try {
+            await signInWithEmailAndPassword(auth, email, password);
+            navigate('/calendar')
+        } catch (err) {
+            setError(err.message);
+        }
+    };
+
     return (
         <div className = 'page'>
             <div className = 'box_login'>
             <h1> Login </h1>
-            <form className='form_login'>
+            <form className='form_login' onSubmit={handleSubmit}>
+                {error && <p>{error}</p>}
                 <label> 
                     Email Address
                     <input 
                         type="email"
                         id="email"
                         placeholder="Email Address"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
                         required 
                     />
                 </label>
@@ -23,6 +45,8 @@ function Login() {
                         type="password"
                         id="password"
                         placeholder="Password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
                         required
                     />
                 </label>
