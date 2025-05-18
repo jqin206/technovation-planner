a1b2c20eddbc53d2502b10c3a0b1dbbc2a8e24c8
June Qin <juneqin2017@gmail.com>
2025-05-17 17:54:04 -0700

    created sign-up page

--- technovation-planner/src/Signup.jsx
+++ technovation-planner/src/Signup.jsx
@@ -1,17 +1,59 @@
 import React from 'react'
+import './Signup.css'
 
 const Signup = () => {
-    console.log("Signup rendered");
     return (
-        <div>
+        <div className = 'page'>
+            <div className = 'box'>
             <h1> Create Account </h1>
             <form>
-                <input type="email" placeholder="Email address" />
-                <input type="text" placeholder="Username" />
-                <input type="password" placeholder="Password" />
-                <button type="submit">Sign Up</button>
+                <label> 
+                    Email Address
+                    <input type="email" placeholder="Email address" />
+                </label>
+                <label>
+                    Username
+                    <input type="text" placeholder="Username" />
+                </label>
+                <label>
+                    Password
+                    <input type="password" placeholder="Password" />
+                </label>
+                <label>
+                    Team Name
+                    <input type="text" placeholder="Team Name" />
+                </label>
+                <label>
+                    Division
+                    <div className="radio-group">
+                        <div className="radio-item">
+                            <input type="radio" name="division" value="beginner" id="beginner" />
+                            <label htmlFor="beginner">Beginner (ages 8-12)</label>
+                        </div>
+                        <div className="radio-item">
+                            <input type="radio" name="division" value="junior" id="junior" />
+                            <label htmlFor="junior">Junior (ages 13-15)</label>
+                        </div>
+                        <div className="radio-item">
+                            <input type="radio" name="division" value="senior" id="senior" />
+                            <label htmlFor="senior">Senior (ages 16-18)</label>
+                        </div>
+                    </div>
+                </label>
+                <label>
+                    Start Date
+                    <input type="date" />
+                </label>
+                <label>
+                    Submission Deadline
+                    <input type="date" />
+                </label>
+                
+                <button type="submit">Create Account</button>
             </form>
         </div>
+    </div>
+        
     )
 }
 
