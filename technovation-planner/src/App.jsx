fe91de221f096c2d96bbc9ce1e705cac7fb07d50
June Qin <juneqin2017@gmail.com>
2025-05-17 22:28:39 -0700

    connected firebase to signup page; created frontend for login

--- technovation-planner/src/App.jsx
+++ technovation-planner/src/App.jsx
@@ -1,17 +1,19 @@
 import { useState } from 'react'
-import {Router, Routes, Route } from 'react-router-dom';
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {Routes, Route } from 'react-router-dom';
 import './App.css'
 import Signup from './Signup'
+import Login from './Login'
 import NavBar from './NavBar'
+import Calendar from './Calendar'
 
 const App = () => {
   return (
     <div>
       <NavBar/>
       <Routes>
+        <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/calendar" element={<Calendar />} />
       </Routes>
     </div>
   );
